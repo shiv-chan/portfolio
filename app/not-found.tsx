4374a97c4086866ee23e74783b93652e2f897015
Kaho Shibuya <kshibuya31@gmail.com>
2024-11-06 12:56:38 -0800

    update not-found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,18 +1,15 @@
-import Link from "next/link";
-import { Button } from "./ui/button";
-import { FaRegFaceFrown } from "react-icons/fa6";
+import PopUp from "./ui/contact/popup";
 
 export default function NotFound() {
 	return (
 		<div className='text-lavender mx-8 pb-20 2xl:pt-8'>
-			<h2 className='text-xl font-bold mb-1'>Page Not Found</h2>
-			<p className='font-light mb-4'>
-				The page you are looking for does not exist...{" "}
-				<FaRegFaceFrown className='inline' />
-			</p>
-			<Link href='/'>
-				<Button>Back to Top</Button>
-			</Link>
+			<PopUp
+				succeed={false}
+				heading='Page Not Found'
+				subText='The page you are looking for does not exist...'
+				buttonText='Back to Top'
+				link='/'
+			/>
 		</div>
 	);
 }
