f122642abe08e34714e9aefcad7d2f3f8e00d45d
Kaho Shibuya <kshibuya31@gmail.com>
2024-11-06 12:43:14 -0800

    build the contact form

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,3 +1,12 @@
+import Form from "../ui/contact/form";
+
 export default function Page() {
-	return <div className='mx-8 pb-20 2xl:pt-8'>Contact Page</div>;
+	return (
+		<div className='mx-8 pb-20 2xl:pt-8'>
+			<h2 className='text-2xl font-bold uppercase text-lavender mb-4'>
+				Love to hear from you!
+			</h2>
+			<Form />
+		</div>
+	);
 }
---
+++ app/ui/contact/form.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { sendEmail } from "@/app/lib/actions";
+import { SubmitMessageButton } from "../button";
+import { useFormState } from "react-dom";
+import ShortUniqueId from "short-unique-id";
+
+export default function Form() {
+	const [state, formAction] = useFormState(sendEmail, null);
+	const uid = new ShortUniqueId({
+		dictionary: "alpha_upper",
+		length: 6,
+	});
+
+	return (
+		<form action={formAction} className='flex flex-col gap-y-4 max-w-lg'>
+			<input name='contact_number' type='hidden' value={uid.rnd()} />
+			<div>
+				<input
+					name='user_name'
+					type='text'
+					placeholder='Your Name'
+					aria-describedby='name-error'
+					className='w-full text-lavender border-2 border-solid border-lavender p-1 placeholder:text-lavender/40'
+				/>
+				{state?.errors.user_name && (
+					<div
+						id='name-error'
+						aria-live='polite'
+						aria-atomic='true'
+						className='mt-1 text-sm text-red-500'
+					>
+						{state.errors.user_name}
+					</div>
+				)}
+			</div>
+			<div>
+				<input
+					name='user_email'
+					type='email'
+					placeholder='Email Address'
+					aria-describedby='email-error'
+					className='w-full text-lavender border-2 border-solid border-lavender p-1 placeholder:text-lavender/40'
+				/>
+				{state?.errors.user_email && (
+					<div
+						id='email-error'
+						aria-live='polite'
+						aria-atomic='true'
+						className='mt-1 text-sm text-red-500'
+					>
+						{state.errors.user_email}
+					</div>
+				)}
+			</div>
+			<div>
+				<textarea
+					name='message'
+					placeholder='Message'
+					aria-describedby='message-error'
+					className='w-full text-lavender border-2 border-solid border-lavender p-1 placeholder:text-lavender/40'
+				></textarea>
+				{state?.errors.message && (
+					<div
+						id='message-error'
+						aria-live='polite'
+						aria-atomic='true'
+						className='text-sm text-red-500'
+					>
+						{state.errors.message}
+					</div>
+				)}
+			</div>
+			<SubmitMessageButton />
+		</form>
+	);
+}
