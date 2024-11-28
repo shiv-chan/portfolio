79f4f16896d6cbd24ccea6b02ebcbac80dfa1a62
Kaho Shibuya <kshibuya31@gmail.com>
2024-11-27 21:16:55 -0800

    add animation to menu

--- app/ui/menu.tsx
+++ app/ui/menu.tsx
@@ -5,25 +5,64 @@ import Image from "next/image";
 import { usePathname } from "next/navigation";
 import clsx from "clsx";
 import SocialMedia from "./socialMedia";
+import { useState, useEffect } from "react";
+import { useNavigationContext } from "@/app/lib/context/navigationContext";
 
 export default function Menu() {
-	const menuItems: string[] = ["works", "about", "contact"];
+	const { previousRoute } = useNavigationContext();
+
+	const [isAnimating, setIsAnimating] = useState<boolean>(false);
+	const [isTitleVisible, setIsTitleVisible] = useState<boolean>(false);
+	const [isMenuVisible, setIsMenuVisible] = useState<boolean[]>([
+		false,
+		false,
+		false,
+	]);
+
+	const menuItems: { menu: string; animation: string }[] = [
+		{ menu: "works", animation: "animate-riseDelay500" },
+		{ menu: "about", animation: "animate-riseDelay1000" },
+		{ menu: "contact", animation: "animate-riseDelay1500" },
+	];
 	const pathname: string | null = usePathname();
 	const currentPage: string | null = pathname.split("/")[1];
-	const menuList = menuItems.map(item => {
+	const menuList = menuItems.map((item, index) => {
+		const { menu, animation } = item;
+		const handleOnAnimationStart = () => {
+			setIsMenuVisible(prev => {
+				prev[index] = true;
+				return [...prev];
+			});
+		};
+
 		return (
 			<li
-				key={item}
+				key={menu}
 				className={clsx([
-					currentPage == `${item}` &&
-						"text-white bg-lavender drop-shadow-selected",
+					{
+						"text-white bg-lavender drop-shadow-selected":
+							currentPage == `${menu}`,
+					},
 					"hover:text-white hover:bg-lavender hover:drop-shadow-selected w-fit",
+					{ [animation]: isAnimating && !previousRoute },
+					`${
+						pathname !== "/" || isMenuVisible[index] || previousRoute
+							? "visible"
+							: "invisible"
+					}`,
 				])}
+				onAnimationStart={handleOnAnimationStart}
 			>
-				<Link href={`/${item}`}>{item}</Link>
+				<Link href={`/${menu}`}>{menu}</Link>
 			</li>
 		);
 	});
+
+	useEffect(() => {
+		if (pathname === "/") {
+			setIsAnimating(true);
+		}
+	}, [pathname]);
 
 	return (
 		<div className='text-lavender md:h-dvh md:col-start-1 2xl:col-start-2 2xl:col-end-3 2xl:relative 2xl:pt-8'>
@@ -35,8 +74,39 @@ export default function Menu() {
 						window.scrollTo(0, 0);
 					}}
 				>
-					<h1 className='text-3xl font-bold uppercase'>Kaho Shibuya</h1>
-					<p className='text-base font-thin'>Software Developer</p>
+					<h1
+						className={clsx(
+							"text-3xl font-bold uppercase",
+							`${
+								pathname !== "/" || isTitleVisible || previousRoute
+									? "visible"
+									: "invisible"
+							}`,
+							{
+								"animate-rise": isAnimating && !previousRoute,
+							}
+						)}
+					>
+						Kaho Shibuya
+					</h1>
+					<p
+						className={clsx(
+							"text-base font-thin",
+							`${
+								pathname !== "/" || isTitleVisible || previousRoute
+									? "visible"
+									: "invisible"
+							}`,
+							{
+								"animate-riseDelay500": isAnimating && !previousRoute,
+							}
+						)}
+						onAnimationStart={() => {
+							setIsTitleVisible(true);
+						}}
+					>
+						Software Developer
+					</p>
 				</Link>
 				<ul className='uppercase font-bold ml-8 pb-8 flex flex-col gap-2.5 text-xl'>
 					{menuList}
