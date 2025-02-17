17915b70a1a01bbe38e8c9ed6e51ea0a27a24f9c
Kaho Shibuya <kshibuya31@gmail.com>
2025-02-16 18:19:01 -0800

    update the layout for the desktop

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -52,7 +52,7 @@ export default async function RootLayout({
 					<NavigationProvider>
 						<OpenToWorkBanner />
 						<Menu />
-						<div className='relative md:mt-8 md:col-span-2 xl:col-start-2 xl:col-end-5 2xl:col-start-3 2xl:col-end-7'>
+						<div className='relative md:mt-8 md:col-span-2 xl:col-start-2 xl:col-end-5 2xl:col-start-3 2xl:col-end-6'>
 							{children}
 						</div>
 						<Footer className="md:hidden self-end" />
